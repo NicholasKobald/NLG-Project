0b3fd683f3e5263bc4fe75087a409bf747e73974
Nicholas Kobald <nick.kobald@gmail.com>
2017-06-09 19:21:35 -0700

    adds report article info

--- code/stats.py
+++ code/stats.py
@@ -3,6 +3,22 @@ import sys
 from utils import DataSet
 
 
+#probably should look at things again after we tokenize
+def report_article_info(training_set):
+    max_article_length = 0
+    min_article_length = float('inf')
+    total_len = 0
+    for key, article in training_set.articles.iteritems():
+        max_article_length = max(len(article), max_article_length)
+        min_article_length = min(min_article_length, len(article))
+        total_len += len(article)
+
+    average_article_length = 1.0 * total_len / len(training_set.articles) * 1.0
+    print "Max article Length:", max_article_length
+    print "Min Article Length:", min_article_length
+    print "Average article length:", average_article_length
+
+
 def report_stance_counts(training_set):
     print "Total Stances: {}".format(len(training_set.stances))
     print "Stance Counts:\n"
@@ -20,6 +36,7 @@ def main():
     d_set = DataSet()
     report_stance_counts(d_set)
     report_article_counts(d_set)
+    report_article_info(d_set)
 
 if __name__=="__main__":
     main()
