070621dd4264617bdb8ea6733bd63e9e010b8774
Brian Chen <ToucheSir@users.noreply.github.com>
2017-07-05 12:36:34 -0700

    Working MLP POC
    Extremely basic, no hidden layers and no proper test/train splitting thus far. Features are just a simple BOW representation.

--- code/main.py
+++ code/main.py
@@ -1,55 +1,52 @@
 import numpy as np
 
-import keras.preprocessing.text
-from keras.preprocessing import sequence
+from keras.preprocessing.text import Tokenizer
 from keras.layers import Input, LSTM, Dense, Activation
 from keras.models import Model, Sequential
-from keras.layers.embeddings import Embedding
+import keras
 
 from utils import DataSet
 
 np.random.seed(7)
 
-stance_id = dict(
-        agree=1,
-        discuss=4,
-        disagree=2,
-        unrelated=3,
-)
+stance_id = {
+    'agree': 0,
+    'discuss': 3,
+    'disagree': 1,
+    'unrelated': 2
+}
 
 def main():
     train_set = DataSet()
     test_set = DataSet('competition_test_bodies.csv',
                        'competition_test_stances.csv')
 
+    # embedding_vector_length = 32
+    max_words = 100
 
-    #embedding_vector_length = 32
-    max_features = 2000
-    max_len = 100
+    tk = Tokenizer(num_words=max_words, lower=True, split=" ")
 
-    tk = keras.preprocessing.text.Tokenizer(
-        num_words=2000,
-        lower=True,
-        split=" "
-    )
+    # parallel lists
+    stances = np.fromiter(map(lambda s: stance_id[s], train_set.triples['stances']),
+                          np.int, len(train_set.triples['stances'])).reshape(-1, 1)
+    articles = train_set.triples['articles']
+    headlines = train_set.triples['headlines']
 
-    #parrallel lists
-    stance_to_num = lambda s: stance_id[s]
-    stances = np.asarray(map(stance_to_num, train_set.triples['stances']))
-    articles = np.asarray(train_set.triples['articles'])
-    headlines = np.asarray(train_set.triples['headlines'])
+    # Train vocab and generate BOW representation
+    tk.fit_on_texts(articles + headlines)
+    bodies_bow = tk.texts_to_matrix(articles)
+    heads_bow = tk.texts_to_matrix(headlines)
 
-    tk.fit_on_texts(articles)
-    trained_seq = tk.texts_to_sequences(articles)
-    trained_seq = sequence.pad_sequences(trained_seq, max_len)
+    train_seq = np.hstack((bodies_bow, heads_bow))
+    print("Trained seq:", train_seq[:5])
+    print("Seq has shape:", train_seq.shape)
 
-    print("Trained seq:", trained_seq[:5])
-    #print("Trained seq:", trained_seq[])
-    print("Seq has shape:", trained_seq.shape)
-    model = Sequential()
-    print("Create Sequential model")
-    #model.add(Embedding(max_features, 100, input_length=max_len))
-    #del.add(LSTM(100))
+    # Converts the N x 1 class vector to a N * classes binary matrix
+    # This is needed to placate keras, for some bizarre reason
+    train_stances = keras.utils.to_categorical(stances, 4)
+
+    # model.add(Embedding(max_features, 100, input_length=max_len))
+    # del.add(LSTM(100))
     # NOTE
     # Trained seq is an embedding vector of the words. it has the shape
     # (49972, 100) since we padded the sequences to the max length.
@@ -62,21 +59,31 @@ def main():
     #
     # https://keras.io/layers/core/
     #
-    #probably worth looking at:
+    # probably worth looking at:
     # https://keras.io/getting-started/sequential-model-guide/
     #
     #
 
-    model.add(Dense(1, input_shape=(None, 100)))
-    #model.add(Activation('sigmoid'))
-    model.compile(loss='binary_crossentropy', optimizer='rmsprop')
-    model.fit(trained_seq, stances,
-        batch_size=32, epochs=10,
-        verbose=1, callbacks=None,
-        validation_split=0.0, validation_data=None,
-        shuffle=True, class_weight=None,
-        sample_weight=None, initial_epoch=0
-    )
+    print("Create Sequential model")
+    model = Sequential()
+    # Input layer takes concatenated BOW vectors
+    model.add(Dense(32, input_shape=(2 * max_words,)))
+    model.add(Activation('relu'))
+    # 4-class outputs - run argmax or on this to get a most probable class
+    model.add(Dense(4))
+    model.add(Activation('softmax'))
+    # model.add(Activation('sigmoid'))
+
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+    model.fit(train_seq, train_stances, batch_size=32, epochs=10, verbose=1, validation_split=0.1, shuffle=True)
+
+    import textwrap
+    test_idx = 0
+    print('~~~ Test ~~~')
+    print('Headline: ', headlines[test_idx])
+    print('Article: ', textwrap.shorten(articles[test_idx], 1000))
+    print('Stance: ', stances[test_idx])
+    print('Predictions: ', model.predict(train_seq[0].reshape(1, -1), batch_size=1))
 
 if __name__ == "__main__":
     main()
