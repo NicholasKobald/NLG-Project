f37865957a1634ceea5561b30f24c7f6e34f313a
Brian Chen <ToucheSir@users.noreply.github.com>
2017-07-18 23:19:29 -0700

    Implement stratification using Pandas (take 2)

--- code/main.py
+++ code/main.py
@@ -1,9 +1,14 @@
 import numpy as np
+import pandas as pd
+
+from functools import reduce
 
 from keras.preprocessing.text import Tokenizer
 from keras.layers import Input, LSTM, Dense, Activation
 from keras.models import Model, Sequential
 import keras
+
+from sklearn.model_selection import train_test_split
 
 from utils import DataSet
 
@@ -16,10 +21,34 @@ stance_id = {
     'unrelated': 2
 }
 
+
+def create_dataset():
+    all_data = pd.read_csv('../data_sets/train_stances.csv')
+    to_join = pd.read_csv('../data_sets/train_bodies.csv')
+    return pd.merge(all_data, to_join)
+
+
+def even_classes(data, sample='min_class'):
+    sample_n = sample
+
+    groups = data.groupby('Stance')
+    counts = groups.size()
+    if sample == 'min_class':
+        sample_n = min(counts)
+    elif sample == 'max_class':
+        sample_n = max(counts)
+
+    sampled = map(lambda g: g[1].sample(sample_n, replace=True), groups)
+    return pd.concat(sampled).reset_index(drop=True)
+
+
 def main():
-    train_set = DataSet()
-    test_set = DataSet('competition_test_bodies.csv',
-                       'competition_test_stances.csv')
+    dataset = create_dataset()
+    train_set, test_set = train_test_split(dataset, stratify=dataset['Stance'])
+    train_set = even_classes(train_set, sample=2000)
+    print('train set:', train_set)
+    # test_set = DataSet('competition_test_bodies.csv',
+    #                    'competition_test_stances.csv')
 
     # embedding_vector_length = 32
     max_words = 100
@@ -27,13 +56,12 @@ def main():
     tk = Tokenizer(num_words=max_words, lower=True, split=" ")
 
     # parallel lists
-    stances = np.fromiter(map(lambda s: stance_id[s], train_set.triples['stances']),
-                          np.int, len(train_set.triples['stances'])).reshape(-1, 1)
-    articles = train_set.triples['articles']
-    headlines = train_set.triples['headlines']
+    stances = train_set['Stance']
+    articles = train_set['articleBody']
+    headlines = train_set['Headline']
 
     # Train vocab and generate BOW representation
-    tk.fit_on_texts(articles + headlines)
+    tk.fit_on_texts(articles.append(headlines))
     bodies_bow = tk.texts_to_matrix(articles)
     heads_bow = tk.texts_to_matrix(headlines)
 
@@ -43,7 +71,9 @@ def main():
 
     # Converts the N x 1 class vector to a N * classes binary matrix
     # This is needed to placate keras, for some bizarre reason
-    train_stances = keras.utils.to_categorical(stances, 4)
+    stance_classes = np.fromiter((stance_id[s] for s in stances),
+                                 np.int, len(stances)).reshape(-1, 1)
+    train_stances = keras.utils.to_categorical(stance_classes, 4)
 
     # model.add(Embedding(max_features, 100, input_length=max_len))
     # del.add(LSTM(100))
@@ -78,12 +108,14 @@ def main():
     model.fit(train_seq, train_stances, batch_size=32, epochs=10, verbose=1, validation_split=0.1, shuffle=True)
 
     import textwrap
-    test_idx = 0
+    test_idx = train_set[train_set['Stance'] == 'unrelated'].index[0]
     print('~~~ Test ~~~')
     print('Headline: ', headlines[test_idx])
     print('Article: ', textwrap.shorten(articles[test_idx], 1000))
     print('Stance: ', stances[test_idx])
-    print('Predictions: ', model.predict(train_seq[0].reshape(1, -1), batch_size=1))
+    pred = model.predict(train_seq[test_idx].reshape(1, -1), batch_size=1)[0]
+    print('Predictions: ', {k: pred[stance_id[k]] for k in stance_id})
+    print('Top prediction: ', np.argmax(pred))
 
 if __name__ == "__main__":
     main()
