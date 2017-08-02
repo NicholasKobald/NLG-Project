760d4f1a274f14ad4b182875a1ce262a78e96ca1
Tahya <tahya.weissgibbons@gmail.com>
2017-08-01 19:57:22 -0700

    added bigrams

--- code/features.py
+++ code/features.py
@@ -5,3 +5,26 @@ def create_bow(articles, headlines, tokenizer, mode='binary'):
     bodies_bow = tokenizer.texts_to_matrix(articles, mode=mode)
     heads_bow = tokenizer.texts_to_matrix(headlines, mode=mode)
     return np.hstack((bodies_bow, heads_bow))
+
+#Given a list of articles/headlines etc computes bigrams
+#Returns list of dict, with each entry the bigram count for associated text
+def compute_bigrams(text):
+	bigrams=[]
+	count=0
+	for j in text:
+		if count == 5:
+			break
+		else:
+			count +=1
+		words={}
+		info=j.split(' ')
+		for i in range(len(info)-1):
+			if info[i]==' ' or info[i+1]==' ':
+				continue
+			word_pair=' '.join(info[i:i+2])
+			if words.has_key(word_pair):
+				words[word_pair] = words[word_pair]+1
+			else:
+				words[word_pair]=1
+		bigrams.append(words)
+	return bigrams
